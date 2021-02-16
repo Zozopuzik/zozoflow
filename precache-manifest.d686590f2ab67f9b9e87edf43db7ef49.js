987735ce74002a9010a76d8c6016dd496a62fcb6
Yevhen Sabadash <ysabadash@e1r12p3.unit.ua>
2021-02-16 16:08:04 +0200

    Updates

---
+++ precache-manifest.d686590f2ab67f9b9e87edf43db7ef49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4b76f6f0cab2096d7ad1460c58fe4c",
+    "url": "/zozoflow/index.html"
+  },
+  {
+    "revision": "e45653d95cd887a4b511",
+    "url": "/zozoflow/static/css/main.2add0cf7.chunk.css"
+  },
+  {
+    "revision": "f3ab208a165bb439961c",
+    "url": "/zozoflow/static/js/2.21fb41ba.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/zozoflow/static/js/2.21fb41ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e45653d95cd887a4b511",
+    "url": "/zozoflow/static/js/main.844e42f4.chunk.js"
+  },
+  {
+    "revision": "5b5f3235ea469863884b",
+    "url": "/zozoflow/static/js/runtime-main.bd813d21.js"
+  }
+]);
